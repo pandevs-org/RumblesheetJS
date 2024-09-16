bcb6dc89df34b5850a3e22225bb11f1d663ea683
Cpytho <89209575+Cpytho@users.noreply.github.com>
2024-09-16 08:52:32 +0530

    fixing formula

--- Scripts/ds.js
+++ Scripts/ds.js
@@ -1,4 +1,4 @@
-import { FormulaParser } from "./formulaparser.js";
+import { FormulaParser } from "./fParser.js";
 class Node {
     constructor(rowValue, colValue, value, nextRow = null, nextCol = null, prevRow = null, prevCol = null) {
         this.rowValue = rowValue;
